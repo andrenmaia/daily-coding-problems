2ffc4f158895535f6afea98c904389509c725578
André Maia <andrenmaia@gmail.com>
2019-04-15 09:12:21 -0300

    Add problem3 dcp description

--- src/main/java/dcp/Problem3.java
+++ src/main/java/dcp/Problem3.java
@@ -5,6 +5,28 @@ import java.lang.*;
 
 public class Problem3 {
 
+    /*
+
+    This problem was asked by Google.
+
+    Given the root to a binary tree, implement serialize(root), which serializes 
+    the tree into a string, and deserialize(s), which deserializes the string back 
+    into the tree.
+
+    For example, given the following Node class
+
+    class Node:
+        def __init__(self, val, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+    The following test should pass:
+
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
+    
+    */
+
     public static class Node {
         public String val;
         public Node left;
